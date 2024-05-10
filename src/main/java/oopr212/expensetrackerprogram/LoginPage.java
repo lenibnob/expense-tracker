7b704749fe844b76da5e498cd7b83e319ac27409
lenibnob <104679128+lenibnob@users.noreply.github.com>
2024-05-10 13:53:34 +0800

    added basic page navigation functions

--- src/main/java/oopr212/expensetrackerprogram/LoginPage.java
+++ src/main/java/oopr212/expensetrackerprogram/LoginPage.java
@@ -24,6 +24,7 @@ public class LoginPage extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel2 = new javax.swing.JPanel();
         jPanel1 = new javax.swing.JPanel();
         username_textfield = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
@@ -33,19 +34,20 @@ public class LoginPage extends javax.swing.JFrame {
         reg_btn = new javax.swing.JButton();
         pass_textfield = new javax.swing.JPasswordField();
         jLabel1 = new javax.swing.JLabel();
-        jPanel2 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Login");
 
+        jPanel2.setBackground(new java.awt.Color(0, 255, 255));
+
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         username_textfield.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        jPanel1.add(username_textfield, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 107, 256, 41));
+        jPanel1.add(username_textfield, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 130, 260, 41));
 
         jLabel3.setFont(new java.awt.Font("Calibri", 1, 14)); // NOI18N
         jLabel3.setText("Password");
-        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 166, -1, -1));
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 190, -1, -1));
 
         login_btn.setBackground(new java.awt.Color(0, 102, 102));
         login_btn.setFont(new java.awt.Font("Calibri", 1, 14)); // NOI18N
@@ -58,15 +60,15 @@ public class LoginPage extends javax.swing.JFrame {
                 login_btnActionPerformed(evt);
             }
         });
-        jPanel1.add(login_btn, new org.netbeans.lib.awtextra.AbsoluteConstraints(108, 251, 123, 45));
+        jPanel1.add(login_btn, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 290, 123, 45));
 
         jLabel2.setFont(new java.awt.Font("Calibri", 1, 14)); // NOI18N
         jLabel2.setText("Username");
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 83, -1, -1));
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 100, -1, -1));
 
         jLabel4.setFont(new java.awt.Font("Calibri", 1, 12)); // NOI18N
         jLabel4.setText("I don't have an account");
-        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 310, -1, -1));
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 360, -1, -1));
 
         reg_btn.setFont(new java.awt.Font("Calibri", 1, 14)); // NOI18N
         reg_btn.setForeground(new java.awt.Color(255, 51, 0));
@@ -78,41 +80,37 @@ public class LoginPage extends javax.swing.JFrame {
                 reg_btnActionPerformed(evt);
             }
         });
-        jPanel1.add(reg_btn, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 330, 79, 40));
-        jPanel1.add(pass_textfield, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 190, 255, 43));
+        jPanel1.add(reg_btn, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 380, 79, 40));
+        jPanel1.add(pass_textfield, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 220, 260, 43));
 
         jLabel1.setBackground(new java.awt.Color(51, 153, 255));
         jLabel1.setFont(new java.awt.Font("Calibri", 1, 30)); // NOI18N
         jLabel1.setText("Log In");
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(136, 23, -1, -1));
-
-        jPanel2.setBackground(new java.awt.Color(0, 255, 255));
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 50, -1, -1));
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 372, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 327, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 405, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 459, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 339, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 399, Short.MAX_VALUE)
-                .addContainerGap())
             .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
@@ -120,11 +118,17 @@ public class LoginPage extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void login_btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_login_btnActionPerformed
-        // TODO add your handling code here:
+        // Go to Main Page
+        dispose();
+        MainPage page = new MainPage();
+        page.show();
     }//GEN-LAST:event_login_btnActionPerformed
 
     private void reg_btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_reg_btnActionPerformed
-        // TODO add your handling code here:
+        // Go to Registration Page
+        dispose();
+        RegistrationPage page = new RegistrationPage();
+        page.show();
     }//GEN-LAST:event_reg_btnActionPerformed
 
     /**
